aef74a439cd40aa134a272ca1d11edc1274057b6
FMAmorim <felipe.amorim@sptech.school>
2024-11-09 11:54:00 -0300

    Começando a desenvolver a tela de Grafico de Qtd De gerentres por empresas

--- web-data-viz/src/models/empresaModel.js
+++ web-data-viz/src/models/empresaModel.js
@@ -13,20 +13,20 @@ function listar() {
 }
 
 function buscarPorCnpj(cnpj) {
-  var instrucaoSql = `SELECT * FROM empresa WHERE cnpj = '${cnpj}'`;
+  var instrucaoSql = `SELECT * FROM empresa WHERE cnpj = '${cnpj};'`;
 
   return database.executar(instrucaoSql);
 }
 
 function cadastrar(nome, cnpj, email) {
-  var instrucaoSql = `insert into Empresa (razaoSocial,cnpj,emailCorporativo) values ('${nome}', ${cnpj}, '${email}')`;
+  var instrucaoSql = `insert into Empresa (razaoSocial,cnpj,emailCorporativo) values ('${nome}', ${cnpj}, '${email}');`;
 
   return database.executar(instrucaoSql);
 }
 
 
 function cadastrarGerente(nome, sobrenome, telefone, email, senha, permissao, empresa) {
-  var instrucaoSql = `insert into Funcionario (nome, sobrenome, numeroTelefone, email, senha, permissao, fkEmpresa) values ('${nome}', '${sobrenome}', ${telefone}, '${email}', '${senha}', '${permissao}', '${empresa}')`;
+  var instrucaoSql = `insert into Funcionario (nome, sobrenome, numeroTelefone, email, senha, permissao, fkEmpresa) values ('${nome}', '${sobrenome}', ${telefone}, '${email}', '${senha}', '${permissao}', '${empresa}');`;
 
   return database.executar(instrucaoSql);
 }
